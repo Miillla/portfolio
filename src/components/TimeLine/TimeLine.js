cd7c273c764650bbc468c851ebe4efa9bd9e36fc
Miillla <p.milana0510@gmail.com>
2023-02-08 02:33:15 +0100

    change file timeline

--- src/components/TimeLine/TimeLine.js
+++ src/components/TimeLine/TimeLine.js
@@ -6,9 +6,11 @@ const TimeLine = () => {
     <Section id="about">
       <SectionTitle>About Me</SectionTitle>
       <SectionText>
-        The purpose of JavaScript Mastery is to help aspiring and established
-        developers to take their development skills to the next level and build
-        awesome apps.
+        I am a tech enthusiast who for the last year has been learning and
+        developing personal projects using React JS and AWS Amplify. I enjoy
+        spending my free-time learning new skills in tech, staying up-to-date,
+        discovering new tools and technologies. I'm interested and open to jobs
+        or internships.
       </SectionText>
     </Section>
   );
