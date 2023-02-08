a0cf41a78465f8284b95fe0281315be7d0505651
Miillla <p.milana0510@gmail.com>
2023-02-08 03:53:19 +0100

    change singel quot

--- src/components/TimeLine/TimeLine.js
+++ src/components/TimeLine/TimeLine.js
@@ -9,7 +9,7 @@ const TimeLine = () => {
         I am a tech enthusiast who for the last year has been learning and
         developing personal projects using React JS and AWS Amplify. I enjoy
         spending my free-time learning new skills in tech, staying up-to-date,
-        discovering new tools and technologies. I'm interested and open to jobs
+        discovering new tools and technologies. I am interested and open to jobs
         or internships.
       </SectionText>
     </Section>
